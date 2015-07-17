4ebb15824199746a50bd61aa77bef8bf86f4fc1e
Rakshith Rao <rakrao@cisco.com>
2015-07-17 12:14:57 +0530

    Tagger cleanup and passing basic test

--- tagger.go
+++ tagger.go
@@ -1,45 +1,39 @@
 package acousticid
 
 import (
-	"io/ioutil"
 	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
 func TagDir(start_dir string) {
+	filepath.Walk(start_dir, tagFile)
+}
 
-	files, err := ioutil.ReadDir(start_dir)
+func tagFile(file string, info os.FileInfo, err error) error {
 
-	if err != nil {
-		panic(err)
-	}
+	if !info.IsDir() && strings.HasSuffix(info.Name(), ".mp3") {
+		fmt.Println("Fingerprinting", file,"...")
+		//Get the fingerprint
+		fingerprint := new(Fingerprint)
+		fingerprint.Get(file)
 
-	for _, file := range files {
-		if strings.HasSuffix(file.Name(), ".mp3") {
-			fmt.Println(file.Name())
-			//Get the fingerprint
-			fingerprint := new(Fingerprint)
-			fingerprint.Get(file.Name())
+		//Get the acoustic id
+		acousticid := new(AcousticidRequest)
+		acousticid.Fingerprint = fingerprint.fingerprint
+		acousticid.Duration = fingerprint.duration
+		acousticid.Apikey = "ULjKruIh"
+		acousticid.Metadata = "recordings"
+		acousticidresponse := acousticid.Request()
 
-			//Get the acoustic id
-			acousticid := new(AcousticidRequest)
-			acousticid.Fingerprint = fingerprint.fingerprint
-			acousticid.Duration = fingerprint.duration
-			acousticid.Apikey = "ULjKruIh"
-			acousticid.Metadata = "recordings"
-			acousticidresponse := acousticid.Request()
-
-			//Is the match good enough?
-			if acousticidresponse.Results[0].Score > 0.9 {
-				id3Tag := new(Id3)
-				id3Tag.Artist = acousticidresponse.Results[0].Recordings[0].Artists[0].Name
-				id3Tag.Title = acousticidresponse.Results[0].Recordings[0].Title
-				id3Tag.set(file.Name())
-			}
-
-
-			//Create an id3 tag and set the id3 tag
-
+		//Is the match good enough?
+		if acousticidresponse.Results[0].Score > 0.9 {
+			id3Tag := new(Id3)
+			id3Tag.Artist = acousticidresponse.Results[0].Recordings[0].Artists[0].Name
+			id3Tag.Title = acousticidresponse.Results[0].Recordings[0].Title
+			id3Tag.set(file)
 		}
 	}
+	return nil
 }
--- tagger_test.go
+++ tagger_test.go
@@ -1,17 +1,26 @@
 package acousticid
 import (
 	"testing"
-	"os"
 	"fmt"
 	"os/exec"
+	"os"
 )
 
 func TestTagger(t *testing.T) {
 	fmt.Println("Setting up test dir...")
-	op, _ := exec.Command("cp", "_testdir", "_testdir_1").Output()
+	//op, err := exec.Command("rm",  "-rf", "_testdir").Output()
+	err := os.RemoveAll("_testdir")
+
+	op, _ := exec.Command("ls",  "-l", "_testdir").Output()
+	fmt.Println(op,err)
+	op, _ = exec.Command("cp", "-r", "_template", "_testdir").Output()
 	fmt.Println(string(op))
 
-	start_dir := os.Getenv("GOPATH") + "/src/github.com/raks81/go-acousticid"
 
+	start_dir := os.Getenv("GOPATH") + "/src/github.com/raks81/go-acousticid/_testdir"
+	fmt.Println(start_dir)
 	TagDir(start_dir)
+
+
+
 }
